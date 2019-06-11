0f223230ca62df5e6bf866d4de70f3cf7d408904
anthony <stackchain@gmail.com>
2019-06-11 11:39:45 -0400

    commit - cookie added

--- routes/api/login.js
+++ routes/api/login.js
@@ -10,6 +10,7 @@ router.post('/', (req, res) => {
         .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({ message: `Welcome ${user.username}!` });
             } else {
                 res.status(401).json({ message: 'Invalid Credentials' });
--- server.js
+++ server.js
@@ -2,13 +2,26 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
 const apiRoute = require('./routes/api/index.js');
 const server = express();
+
+const sessionConfig = {
+    name: 'football',
+    secret: '4th and goal',
+    cookie: {
+        maxAge: 10000 * 30,
+        secure: false, // true in production
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false, //Check law - get permission
+};
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
+server.use(session(sessionConfig));
 
 
 server.get('/', (req, res) => {
